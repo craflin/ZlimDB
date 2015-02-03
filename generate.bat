1eccc7e22cb0420d6a7db73e5103bb83880f2364
Colin Graf <colin.graf@sovereign-labs.com>
2015-02-03 21:37:39 +0100

    Update mare

--- generate.bat
+++ generate.bat
@@ -1,5 +1,5 @@
 @echo off
 
-if not exist Build\mare\mare.exe call Ext\mare\build.bat --buildDir=Build/mare --outputDir=Build/mare --sourceDir=Ext/mare/src
+if not exist Build\mare\mare.exe call Ext\mare\compile.bat --buildDir=Build/mare --outputDir=Build/mare --sourceDir=Ext/mare/src
 if not "%1"=="" (Build\mare\mare.exe %*) else Build\mare\mare.exe --vcxproj=2013
 
