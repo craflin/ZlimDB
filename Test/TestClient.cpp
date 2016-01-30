73f72d6ceb035cdad4c857f2fd5fb2516387f686
Colin Graf <colin.graf@sovereign-labs.com>
2016-01-30 20:27:23 +0100

    Added table clearing test

--- Test/TestClient.cpp
+++ Test/TestClient.cpp
@@ -94,9 +94,38 @@ void_t testClient(const char_t* argv0)
       ASSERT(j == 10000);
     }
 
-    ASSERT(zlimdb_free(zdb) == 0);
-    ASSERT(zlimdb_cleanup() == 0);
   }
+
+  // test clear
+  {
+    uint32_t tableId;
+    ASSERT(zlimdb_add_table(zdb, "TestTableClear", &tableId) == 0);
+
+    char_t buffer[ZLIMDB_MAX_MESSAGE_SIZE];
+    zlimdb_entity* entity = (zlimdb_entity*)buffer;
+    entity->size = 123;
+    entity->id = 0;
+    entity->time = 0;
+    uint64_t entityId;
+    ASSERT(zlimdb_add(zdb, tableId, entity, &entityId) == 0);
+    ASSERT(zlimdb_add(zdb, tableId, entity, &entityId) == 0);
+    ASSERT(zlimdb_add(zdb, tableId, entity, &entityId) == 0);
+    ASSERT(zlimdb_clear(zdb, tableId) == 0);
+    ASSERT(zlimdb_add(zdb, tableId, entity, &entityId) == 0);
+    ASSERT(zlimdb_add(zdb, tableId, entity, &entityId) == 0);
+    ASSERT(zlimdb_add(zdb, tableId, entity, &entityId) == 0);
+    ASSERT(zlimdb_add(zdb, tableId, entity, &entityId) == 0);
+    ASSERT(zlimdb_query(zdb, tableId, zlimdb_query_type_all, 0) == 0);
+    size_t count = 0;
+      while(zlimdb_get_response(zdb, (zlimdb_header*)buffer, sizeof(buffer)) == 0)
+        for(const zlimdb_entity* entity = zlimdb_get_first_entity((zlimdb_header*)buffer, sizeof(zlimdb_entity)); entity; entity = zlimdb_get_next_entity((zlimdb_header*)buffer, sizeof(zlimdb_entity), entity))
+          ++count;
+    ASSERT(count == 4);
+  }
+
+  // close connection
+  ASSERT(zlimdb_free(zdb) == 0);
+  ASSERT(zlimdb_cleanup() == 0);
 
   // terminate zlimdb server
   ASSERT(zlimdbServer.kill());
